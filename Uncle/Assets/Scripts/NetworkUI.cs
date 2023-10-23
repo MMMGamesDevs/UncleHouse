ab591345af35aac7ee61d92c833c6cf326674c1d
mateteCode <matete@gmail.com>
2023-10-23 20:47:26 -0300

    puertas abiertas para exploracion

--- Uncle/Assets/Scripts/NetworkUI.cs
+++ Uncle/Assets/Scripts/NetworkUI.cs
@@ -9,11 +9,13 @@ public class NetworkUI : MonoBehaviour
     [SerializeField] Button serverBtn;
     [SerializeField] Button hostBtn;
     [SerializeField] Button clientBtn;
+    [SerializeField] Button exitBtn;
 
     private void Awake()
     {
         serverBtn.onClick.AddListener(() => NetworkManager.Singleton.StartServer());
         hostBtn.onClick.AddListener(() => NetworkManager.Singleton.StartHost());
         clientBtn.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
+        exitBtn.onClick.AddListener(() => Application.Quit());
     }
 }
