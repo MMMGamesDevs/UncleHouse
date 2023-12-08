5d6b1b366c634638c9802256df03dd33e8b64b25
WalterM <98977639+Mosow357@users.noreply.github.com>
2023-12-08 13:52:52 -0300

    UI script

--- Uncle/Assets/_Walter/UI/MenuController.cs
+++ Uncle/Assets/_Walter/UI/MenuController.cs
@@ -8,20 +8,25 @@ public class MenuController : MonoBehaviour
 {
     [SerializeField]
     private VisualTreeAsset _character_Menu;
+    private VisualElement _CharactersBtns;
+    private VisualElement _btnWrapper;
 
     private UIDocument _doc;
     private Button _startButton;
     private Button _optionsButton;
     private Button _quitButton;
 
-    private VisualElement _CharactersBtns;
-    private VisualElement _btnWrapper;
+    
 
     private void Awake()
     {
         _doc = GetComponent<UIDocument>();
         _startButton = _doc.rootVisualElement.Q<Button>("StartBtn");
+
+        _character_Menu = Instantiate(_character_Menu);
+
         _CharactersBtns = _doc.rootVisualElement.Q<VisualElement>("CharactersBtns");
+        _btnWrapper = _doc.rootVisualElement.Q<VisualElement>("BtnWrapper");
         _startButton.clicked += StartButtonOnClicked; 
 
 
